5d0c1997714196e920a14cbf2d54fb6f632ec4fe
student <student@DESKTOP-KUGQ5PI>
2025-10-01 14:23:12 +0500

    лаба 5

--- Yakupov_AutoService/ServicePage2.xaml.cs
+++ Yakupov_AutoService/ServicePage2.xaml.cs
@@ -25,11 +25,81 @@ namespace Yakupov_AutoService
             InitializeComponent();
             var currentServices = ЯкуповаАвтосервисEntities.GetContext().Service.ToList();
             ServiceListView.ItemsSource = currentServices;
+            ComboType.SelectedIndex = 0;
+            UpdateServices();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+        private void ComboType_SelectionChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+        private void UpdateServices()
+        {
+            var currentServices = ЯкуповаАвтосервисEntities.GetContext().Service.ToList();
+            if (ComboType.SelectedIndex == 0)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.Discount) >= 0 && Convert.ToInt32(p.Discount) <= 100)).ToList();
+            }
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.Discount) >= 0 && Convert.ToInt32(p.Discount) < 5)).ToList();
+
+            }
+
+            if(ComboType.SelectedIndex == 2)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.Discount) >= 5 && Convert.ToInt32(p.Discount) < 15)).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.Discount) >= 15 && Convert.ToInt32(p.Discount) < 30)).ToList();
+            }
+            if (ComboType.SelectedIndex == 4)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.Discount) >= 30 && Convert.ToInt32(p.Discount) < 70)).ToList();
+            }
+            if (ComboType.SelectedIndex == 5)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.Discount) >= 70 && Convert.ToInt32(p.Discount) < 100)).ToList();
+            }
+            currentServices = currentServices.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            ServiceListView.ItemsSource = currentServices.ToList();
+
+            if(RButtonDown.IsChecked.Value)
+            {
+                ServiceListView.ItemsSource = currentServices.OrderByDescending(p => p.Cost).ToList();
+
+            }
+
+            if(RButtonUp.IsChecked.Value)
+            {
+                ServiceListView.ItemsSource = currentServices.OrderBy(p => p.Cost).ToList();
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEdit());
+        }
+
+
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
         }
     }
 }
